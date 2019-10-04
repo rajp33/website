c3cb356e377e6a9bb0909fbf51b6235321a0e3f3
rpatel3318 <patel.raj2@husky.neu.edu>
2019-10-04 11:57:11 -0400

    update

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var http = require('http')
-var fs = require('fs')
+// var http = require('http')
+// var fs = require('fs')
+//
+// http.createServer(onRequest).listen(8888)
+// console.log('Server has started')
+//
+// function onRequest(request, response){
+//   switch (request.url) {
+//     case "/css/master.css" :
+//       fs.readFile('css/master.css', function(err, data) {
+//         response.writeHead(200, {'Content-Type' : 'text/css', 'Content-Length': data.length});
+//         response.write(data);
+//         response.end();
+//       })
+//       break
+//     default:
+//       fs.readFile('index.html', function(err, data) {
+//         response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+//         response.write(data);
+//         response.end();
+//       })
+//   }
+// }
 
-http.createServer(onRequest).listen(8888)
-console.log('Server has started')
+var express = require('express')
+var app = express()
 
-function onRequest(request, response){
-  fs.readFile('index.html', function(err, data) {
-    response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-    response.write(data);
-    response.end();
-  })
-}
+app.use(express.static('public'))
+
+var server = app.listen(8000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Express app listening at http://%s:%s', host, port)
+
+})
