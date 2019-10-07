fda25fa69d2a29f1c391bda642d7c89a95054d0e
rpatel3318 <patel.raj2@husky.neu.edu>
2019-10-07 17:25:17 -0400

    project page update

--- public/js/projectPage.js
+++ public/js/projectPage.js
@@ -3,28 +3,32 @@ listdata = {
     "description" : "A Note taking application that aims to use NLP and Machine Learning to conver notes into reincfcement learning questions specifically for students",
     "technologies" : ["NLP", "Swift", "ML", "RNN", "Python"],
     "startDate" : "",
-    "enddate" : ""
+    "enddate" : "",
+    "imgSrc" : ""
   },
 
   "SneakAssist" : {
     "description" : "A subscription based application that provides support for limited edition sneaker releases.",
     "technologies" : ["HTML", "CSS", "JS", "MongoDB", "Python"],
     "startDate" : "",
-    "endDate" : ""
+    "endDate" : "",
+    "imgSrc" : ""
   },
 
   "LinkedIn Scraper" : {
     "description" : "A script (ported to Java) to scrape LinkedIn for the purpose of gathering data about alumni at Northeastern.",
     "technologies" : ["Java", "JavaFX", "Selenium"],
     "startDate" : "",
-    "endDate" : ""
+    "endDate" : "",
+    "imgSrc" : ""
   },
 
   "Animator" : {
     "description" : "A simple animation viewer and editor to reinforce OOD principles. Built for CS3500: Object Oriented Design.",
     "technologies" : ["Java", "Swing"],
     "startDate" : "",
-    "endDate" : ""
+    "endDate" : "",
+    "imgSrc" : ""
   },
 
   "Mini-Games" : {
@@ -32,6 +36,7 @@ listdata = {
     "technologies" : ["Java"],
     "startDate" : "",
     "endDate" : "",
+    "imgSrc" : ""
   },
 
   "Music Player" : {
@@ -39,6 +44,7 @@ listdata = {
     "technologies" : ["Racket"],
     "startDate" : "",
     "endDate" : "",
+    "imgSrc" : ""
   }
 }
 
@@ -48,13 +54,28 @@ function populateList(name, data) {
     if (data.hasOwnProperty(project)) {
       //create List item
       var listItem = document.createElement("LI")
-      listItem.textContent = project
       ulElement.appendChild(listItem)
-      //create paragraph item
-      var desc = document.createElement("P")
-      desc.textContent = data[project]["description"]
-      listItem.appendChild(desc)
       //create info Container
+      var info = document.createElement("DIV")
+      info.class = "cardInfo"
+      var name = document.createElement("P")
+
+      listItem.appendChild(info)
+
+      if (data[project]["imgSrc"] != "") { // if Image available
+        var img = document.createElement("IMG")
+        img.src = data[project]["imgSrc"]
+        info.appendChild(img)
+      } else {
+        //If no Image Available
+      }
+
+      name.textContent = project
+      info.appendChild(name)
+      //create paragraph item
+      // var desc = document.createElement("P")
+      // desc.textContent = data[project]["description"]
+      //listItem.appendChild(desc)
     }
   }
 }
