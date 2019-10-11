71014a8a48e1c0f5475ea52e4bc2c0ed455cd033
rpatel3318 <patel.raj2@husky.neu.edu>
2019-10-10 21:03:47 -0400

    update project page, update css horizontal align

--- public/js/projectPage.js
+++ public/js/projectPage.js
@@ -1,6 +1,6 @@
 listdata = {
   "Deck" : {
-    "description" : "A Note taking application that aims to use NLP and Machine Learning to conver notes into reincfcement learning questions specifically for students",
+    "description" : "A Note taking application that aims to use NLP and Machine Learning to convert notes into reinforcement learning questions specifically for students",
     "technologies" : ["NLP", "Swift", "ML", "RNN", "Python"],
     "startDate" : "",
     "enddate" : "",
@@ -48,37 +48,79 @@ listdata = {
   }
 }
 
+//create a tile container and append it to the given webelement
+function createRow(webElement) {
+  var row = document.createElement("DIV")
+  row.classList.add("tile")
+  webElement.appendChild(row)
+  return row
+}
+
+function createGrid(data, maxCol, parent) {
+  var row_count = 0
+  //for each data create tile
+  for (var project in data) {
+    //change row
+    if (row_count % maxCol == 0) {
+      currentRow = createRow(parent)
+    }
+
+    var tileparent = document.createElement("DIV")
+    tileparent.classList.add("tile", "is-parent")
+    var article = document.createElement("ARTICLE")
+    article.classList.add("tile", "is-child", "box")
+
+    var name = document.createElement("P")
+    name.textContent = project
+    article.appendChild(name)
+
+    var desc = document.createElement("P")
+    desc.textContent = data[project]["description"]
+    desc.classList.add("subtitle")
+    article.appendChild(desc)
+
+    tileparent.appendChild(article)
+    currentRow.appendChild(tileparent)
+    row_count++
+  }
+}
+
 function populateList(name, data) {
   const ulElement = document.querySelector(".projectList")
-  for (var project in data) {
-    if (data.hasOwnProperty(project)) {
       //create List item
-      var listItem = document.createElement("LI")
-      ulElement.appendChild(listItem)
-      //create info Container
-      var info = document.createElement("DIV")
-      info.classList.add("cardInfo")
-      var name = document.createElement("P")
-      name.classList.add("cardName")
+      // var listItem = document.createElement("DIV")
+      // listItem.classList.add("tile", "is-parent")
+      // ulElement.appendChild(listItem)
+      // //create info Container
+      // var info = document.createElement("DIV")
+      // info.classList.add("tile", "cardInfo")
+      // var name = document.createElement("P")
+      // name.classList.add("title", "cardName")
+      //
+      // listItem.appendChild(info)
+      //
+      // if (data[project]["imgSrc"] != "") { // if Image available
+      //   var img = document.createElement("IMG")
+      //   img.src = data[project]["imgSrc"]
+      //   info.appendChild(img)
+      // } else {
+      //   //If no Image Available
+      // }
+      //
+      // name.textContent = project
+      // info.appendChild(name)
+      // //create paragraph item
+      // // var desc = document.createElement("P")
+      // // desc.textContent = data[project]["description"]
+      // //listItem.appendChild(desc)
 
-      listItem.appendChild(info)
+      //create a vertical container for the element
+      var vertical = createRow(ulElement)
+      vertical.classList.add("is-vertical")
 
-      if (data[project]["imgSrc"] != "") { // if Image available
-        var img = document.createElement("IMG")
-        img.src = data[project]["imgSrc"]
-        info.appendChild(img)
-      } else {
-        //If no Image Available
-      }
-
-      name.textContent = project
-      info.appendChild(name)
-      //create paragraph item
-      // var desc = document.createElement("P")
-      // desc.textContent = data[project]["description"]
-      //listItem.appendChild(desc)
-    }
-  }
+      createGrid(data, 3, vertical)
+      //create initial row
+      //var currentRow = createRow(vertical)
 }
 
 populateList("projectList", listdata)
