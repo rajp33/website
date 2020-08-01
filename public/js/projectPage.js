ae47693fac826f1666cfee48d7f05a9684c40160
Raj <rpatel3318@gmail.com>
2020-08-01 18:21:57 -0400

    Update

--- public/js/projectPage.js
+++ public/js/projectPage.js
@@ -2,8 +2,8 @@
 //create a tile container and append it to the given webelement
 function createRow(webElement) {
   var row = document.createElement("DIV")
-  row.classList.add("tile")
-  webElement.appendChild(row)
+  row.classList.add("tile");
+  webElement.appendChild(row);
   return row
 }
 
@@ -19,7 +19,7 @@ function showDialog(event) {
 }
 
 function createGrid(data, maxCol, parent) {
-  var row_count = 0
+  var row_count = 0;
   //for each data create tile
   for (var project in data) {
     //change row
@@ -27,28 +27,28 @@ function createGrid(data, maxCol, parent) {
       currentRow = createRow(parent)
     }
 
-    var tileparent = document.createElement("DIV")
-    tileparent.classList.add("tile", "is-parent")
-    var article = document.createElement("ARTICLE")
-    article.classList.add("tile", "is-child", "box")
-    article.onClick = showDialog
+    var tileparent = document.createElement("DIV");
+    tileparent.classList.add("tile", "is-parent");
+    var article = document.createElement("ARTICLE");
+    article.classList.add("tile", "is-child", "box");
+    article.onClick = showDialog;
 
-    var name = document.createElement("P")
-    name.classList.add("name")
-    name.textContent = project
-    article.appendChild(name)
+    var name = document.createElement("P");
+    name.classList.add("name");
+    name.textContent = project;
+    article.appendChild(name);
 
-    var desc = document.createElement("P")
-    desc.textContent = data[project]["description"]
-    desc.classList.add("subtitle")
+    var desc = document.createElement("P");
+    desc.textContent = data[project]["description"];
+    desc.classList.add("subtitle");
     article.appendChild(desc)
 
     if (data[project]["imgSrc"] != "") { // if Image available
-      var figure = document.createElement("FIGURE")
-      figure.classList.add("image")
-      var img = document.createElement("IMG")
-      img.src = data[project]["imgSrc"]
-      figure.appendChild(img)
+      var figure = document.createElement("FIGURE");
+      figure.classList.add("image");
+      var img = document.createElement("IMG");
+      img.src = data[project]["imgSrc"];
+      figure.appendChild(img);
       article.appendChild(figure)
     }
 
@@ -59,7 +59,7 @@ function createGrid(data, maxCol, parent) {
 }
 
 function populateList(name, data) {
-  const ulElement = document.querySelector(".projectList")
+  const ulElement = document.querySelector(".projectList");
       //create List item
       // var listItem = document.createElement("DIV")
       // listItem.classList.add("tile", "is-parent")
@@ -81,14 +81,14 @@ function populateList(name, data) {
       // //listItem.appendChild(desc)
 
       //create a vertical container for the element
-      var vertical = createRow(ulElement)
-      vertical.classList.add("is-vertical")
+      var vertical = createRow(ulElement);
+      vertical.classList.add("is-vertical");
 
       createGrid(data, 3, vertical)
       //create initial row
       //var currentRow = createRow(vertical)
 }
 
-var listdata = getJSON("../../listdata.json")
+var listdata = getJSON("../listdata.json");
 
-populateList("projectList", listdata)
+populateList("projectList", listdata);
